9e04f21d64316d18e24c0f064afe32f933c03794
kahbeer42 <olabodex2m@gmail.com>
2023-03-07 06:04:20 -0800

    Print ramdom number

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+number = random.randint(-10, 10)
 if number > 0:
     print(number, 'is positive')
-elif number < 0:
-    print(number, 'is positive')
-else:
+elif number == 0:
     print(number, 'is zero')
+else:
+    print(number, 'is negative')
