0899ece27ff68f053c125d79b29b12985ca41a6d
kahbeer42 <olabodex2m@gmail.com>
2023-04-12 10:37:10 -0700

    Input and Output project

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,20 +1,3 @@
-kip to content
-ephayeabe
-/
-alx-higher_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-higher_level_programming/0x0B-python-input_output/1-write_file.py
-@ephayeabe
-ephayeabe create 1-write_file.py
- 1 contributor
-10 lines (8 sloc)  257 Bytes
 #!/usr/bin/python3
 """
 Contains the function "wrtie_file"
