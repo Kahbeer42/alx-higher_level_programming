8894d5a2c7ec54fbb28028f5de64b83cfdb6acc6
kahbeer42 <olabodex2m@gmail.com>
2023-06-14 12:04:27 -0700

    Javascript warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
+
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
+}
